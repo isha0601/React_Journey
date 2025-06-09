668bd36593e68fa66aa9cdcbacc40064f9e55715
Isha Komervelliwar <ishakomervelliwar0601@gmail.com>
2025-06-10 00:26:37 +0530

    Add files via upload

---
+++ src/components/PostList.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect,useState } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setFetching(true);
+    fetch("https://dummyjson.com/posts",{signal})
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      });
+      return ()=>{
+        controller.abort();
+      }
+  }, []);
+
+  return (
+    <>
+    {fetching && <LoadingSpinner/>}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      { !fetching && postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+export default PostList;
